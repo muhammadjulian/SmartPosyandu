a9dd11b7abbbf4fdacac076da521e1a8af79eadc
Fikrifahmiamin <fikrifahmiamin19@gmail.com>
2019-07-09 21:24:34 -0700

    Update
    Perkembangan Chart dan penambahan Dashboard

--- application/views/templates/topbar.php
+++ application/views/templates/topbar.php
@@ -6,15 +6,7 @@
 
         <!-- Topbar -->
         <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
-
             <ul class="navbar-nav ml-auto">
-
-
-
-
-
-
                 <div class="topbar-divider d-none d-sm-block"></div>
 
                 <!-- Nav Item - User Information -->
